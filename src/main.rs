7ef02b71dac19230a7d84c89abc6b704eda89c5e
Or Grinberg <or.grin@gmail.com>
2022-12-22 19:33:09 +0000

    Add a basic tweet

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
-use reqwest::Client;
-use reqwest_oauth1;
+use dotenv::dotenv;
+use twitter_v2::authorization::Oauth1aToken;
+use twitter_v2::TwitterApi;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // twitter client initialization
-    // let secrets = reqwest_oauth1::Secrets::new(consumer_key, consumer_secret)
-    //     .token(access_token, token_secret);
-    // let client = Client::new();
-    // let resp = client
-    //     .get("https://api.twitter.com/2/tweets/search/recent?query=from:twitterdev")
-    //     // .oauth1(secrets)
-    //     .send()
-    //     .await?;
-    // println!("{}", resp.text().await.unwrap());
+    dotenv().ok();
+    let consumer_key = std::env::var("CONSUMER_KEY").expect("CONSUMER_KEY must be set.");
+    let consumer_secret = std::env::var("CONSUMER_SECRET").expect("CONSUMER_SECRET must be set.");
+    let access_token = std::env::var("ACCESS_TOKEN").expect("ACCESS_TOKEN must be set.");
+    let token_secret = std::env::var("TOKEN_SECRET").expect("TOKEN_SECRET must be set.");
+
+    let auth = Oauth1aToken::new(consumer_key, consumer_secret, access_token, token_secret);
+
+    let api = TwitterApi::new(auth);
+    let resp = api
+        .post_tweet()
+        .text("test tweet 2".to_string())
+        .send()
+        .await?;
+    println!("{}", resp.data().unwrap().id);
     Ok(())
 }
