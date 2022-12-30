1707985eac583c89da5f41f684cf7e360e54d088
Or Grinberg <or.grin@gmail.com>
2022-12-30 18:26:36 +0100

    Communicate with kaspad, get accepted blocks on a stream, filter out largest output from every tx in the block.

--- src/kaspa_handler.rs
+++ src/kaspa_handler.rs
@@ -1,3 +1,138 @@
+use crate::proto::kaspad_message::Payload;
 use crate::proto::rpc_client::RpcClient;
+use crate::proto::{
+    BlockAddedNotificationMessage, GetInfoRequestMessage, KaspadMessage,
+    NotifyBlockAddedRequestMessage, RpcBlockVerboseData,
+};
 
-pub struct KaspaHandler {}
+use crate::Error;
+use tokio::sync::mpsc::{self, Sender};
+use tokio_stream::wrappers::ReceiverStream;
+use tonic::transport::{Channel, Endpoint};
+use tonic::Streaming;
+
+pub struct KaspaHandler {
+    client: RpcClient<Channel>,
+    stream: Streaming<KaspadMessage>,
+    send_channel: Sender<KaspadMessage>,
+}
+
+impl KaspaHandler {
+    pub async fn connect<A>(address: A) -> Result<Self, Error>
+    where
+        A: std::convert::TryInto<Endpoint>,
+        A::Error: Into<Error>,
+    {
+        let mut client = RpcClient::connect(address).await?;
+        let (send_channel, recv) = mpsc::channel(3);
+        send_channel.send(GetInfoRequestMessage {}.into()).await?;
+        let stream = client
+            .message_stream(ReceiverStream::new(recv))
+            .await?
+            .into_inner();
+        Ok(Self {
+            client,
+            stream,
+            send_channel,
+        })
+    }
+
+    pub async fn listen(&mut self) -> Result<(), Error> {
+        loop {
+            while let Some(msg) = self.stream.message().await? {
+                match msg.payload {
+                    Some(payload) => self.handle_message(payload).await?,
+                    None => println!("kaspad message payload is empty"),
+                }
+            }
+        }
+    }
+
+    async fn handle_message(&self, message: Payload) -> Result<(), Error> {
+        match message {
+            Payload::GetInfoResponse(info) => {
+                println!("Kaspad version: {}", info.server_version);
+                self.send_channel
+                    .send(NotifyBlockAddedRequestMessage {}.into())
+                    .await?;
+            }
+            Payload::NotifyBlockAddedResponse(response_message) => {
+                if let Some(err) = response_message.error {
+                    return Err(err.message.into());
+                }
+            }
+            Payload::BlockAddedNotification(block_added_notif_message) => {
+                return self.handle_new_block(block_added_notif_message).await;
+            }
+            _ => println!("unrelated message received: {:?}", message),
+        }
+        Ok(())
+    }
+
+    async fn handle_new_block(
+        &self,
+        block_notif: BlockAddedNotificationMessage,
+    ) -> Result<(), Error> {
+        println!("new block notified!",);
+        let verbose_data = extract_verbose_data(&block_notif)?;
+        if !verbose_data.is_chain_block {
+            println!("not part of chain");
+            return Ok(());
+        }
+
+        let txs = block_notif.block.unwrap().transactions;
+        for tx in txs.iter() {
+            if tx.inputs.len() == 0 {
+                continue;
+            }
+            println!("{}", tx.inputs.len());
+            println!("{}", tx.outputs.len());
+            tx.outputs.iter().map(|op| {
+                println!(
+                    "output sig: {}",
+                    op.script_public_key.as_ref().unwrap().script_public_key
+                );
+            });
+            let total_output: u64 = tx.outputs.iter().map(|op| op.amount).sum();
+            let max_output: u64 = tx.outputs.iter().map(|op| op.amount).max().unwrap();
+            println!(
+                "tx total output amount {}, largest output amount {}",
+                total_output, max_output
+            );
+        }
+
+        println!("accepted block!");
+
+        Ok(())
+    }
+}
+
+impl From<GetInfoRequestMessage> for KaspadMessage {
+    #[inline(always)]
+    fn from(a: GetInfoRequestMessage) -> Self {
+        KaspadMessage {
+            payload: Some(Payload::GetInfoRequest(a)),
+        }
+    }
+}
+
+impl From<NotifyBlockAddedRequestMessage> for KaspadMessage {
+    fn from(a: NotifyBlockAddedRequestMessage) -> Self {
+        KaspadMessage {
+            payload: Some(Payload::NotifyBlockAddedRequest(a)),
+        }
+    }
+}
+
+fn extract_verbose_data(
+    block_notif: &BlockAddedNotificationMessage,
+) -> Result<RpcBlockVerboseData, Error> {
+    let err_message = "failed to extract verbose data from block";
+    match &block_notif.block {
+        Some(block) => match &block.verbose_data {
+            Some(verbose_data) => return Ok(verbose_data.clone()),
+            None => Err(err_message.into()),
+        },
+        None => Err(err_message.into()),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,44 @@
+mod kaspa_handler;
 mod twitter;
 
 use dotenv::dotenv;
-use std::error::Error;
+use kaspa_handler::KaspaHandler;
 use twitter::TwitterKeys;
 
 pub mod proto {
     tonic::include_proto!("protowire");
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    dotenv().ok();
-    let consumer_key = parse_env_var("CONSUMER_KEY")?;
-    let consumer_secret = parse_env_var("CONSUMER_SECRET")?;
-    let access_token = parse_env_var("ACCESS_TOKEN")?;
-    let token_secret = parse_env_var("TOKEN_SECRET")?;
-    let kaspad_address = parse_env_var("KASPAD_ADDRESS")?;
+use std::{error::Error as StdError, net::IpAddr, str::FromStr};
 
-    let twitter_keys = TwitterKeys::new(consumer_key, consumer_secret, access_token, token_secret);
-    let message = "Whale alert".to_string();
-    twitter_keys.tweet(message).await
+pub type Error = Box<dyn StdError + Send + Sync + 'static>;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    dotenv().ok();
+    let consumer_key = parse_env_var("CONSUMER_KEY");
+    let consumer_secret = parse_env_var("CONSUMER_SECRET");
+    let access_token = parse_env_var("ACCESS_TOKEN");
+    let token_secret = parse_env_var("TOKEN_SECRET");
+    let mut kaspad_address = parse_env_var("KASPAD_ADDRESS");
+    let port = parse_env_var("KASPAD_PORT");
+    if !kaspad_address.starts_with("grpc://") {
+        IpAddr::from_str(&kaspad_address)?;
+        kaspad_address = format!("grpc://{}:{}", kaspad_address, port);
+    }
+
+    // let twitter_keys = TwitterKeys::new(consumer_key, consumer_secret, access_token, token_secret);
+    // let message = "Whale alert".to_string();
+    // twitter_keys.tweet(message).await
+    loop {
+        let mut client = KaspaHandler::connect(kaspad_address.clone()).await?;
+        client.listen().await?;
+    }
+    println!("done listening");
+    Ok(())
 }
 
-fn parse_env_var(var_name: &str) -> Result<String, Box<dyn Error>> {
+fn parse_env_var(var_name: &str) -> String {
     let err_message = format!("{} must be set.", var_name);
-    let result = std::env::var(var_name).expect(&err_message);
-    Ok(result)
+    std::env::var(var_name).expect(&err_message)
 }
